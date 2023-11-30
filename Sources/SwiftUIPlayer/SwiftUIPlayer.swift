80bcd7e4c39801ffc6ff9050ce53cfb4fcd8fdf8
 Aitolkun Anarbekova <tokusha.aa@gmail.com>
2023-11-30 10:59:42 +0300

    commit

--- Sources/SwiftUIPlayer/SwiftUIPlayer.swift
+++ Sources/SwiftUIPlayer/SwiftUIPlayer.swift
@@ -2,14 +2,17 @@
 // https://docs.swift.org/swift-book
 
 import SwiftUI
+import AVKit
 
 @available(iOS 14.0, *)
 public struct SwiftUIPlayer: View {
     
     @ObservedObject var viewModel: PlayerViewModel
     
-    public init(url: String, timecodes: [Timecode]) {
-        self.viewModel = PlayerViewModel(url: url, timecodes: timecodes)
+    public init(url: String, timecodes: [Timecode]?) {
+        self.viewModel = PlayerViewModel(
+            url: url,
+            timecodes: timecodes ?? [Timecode(title: "", time:  CMTime(seconds: 10, preferredTimescale: 1))])
     }
     
     public var body: some View {
